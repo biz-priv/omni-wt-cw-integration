7f2d7f0e1bdd9783a3fc3babdea35a19acae3c04
Mohammed Sazeed <mohammed.sazeed@bizcloudexperts.com>
2024-05-16 22:58:26 +0530

    feat: initial commit

--- src/shared/helper.js
+++ src/shared/helper.js
@@ -1,6 +1,10 @@
 'use strict'
 
+const AWS = require('aws-sdk')
 const momentTZ = require('moment-timezone');
+const xml2js = require('xml2js');
+
+const s3 = new AWS.S3();
 
 const cstDateTime = momentTZ.tz('America/Chicago').format('YYYY/MM/DD HH:mm:ss').toString();
 
@@ -11,7 +15,32 @@ const STATUSES = {
   FAILED: 'FAILED',
 };
 
+async function getS3Object(bucket, key) {
+  try {
+    const params = { Bucket: bucket, Key: key };
+    const response = await s3.getObject(params).promise();
+    return response.Body.toString();
+  } catch (error) {
+    throw new Error(`S3 error: ${error}`);
+  }
+}
+
+async function xmlToJson(xmlData) {
+  try {
+    const parser = new xml2js.Parser({
+      explicitArray: false,
+      mergeAttrs: true,
+    });
+    return await parser.parseStringPromise(xmlData);
+  } catch (error) {
+    console.error('Error in xmlToJson: ', error);
+    throw error;
+  }
+}
+
 module.exports = {
   cstDateTime,
-  STATUSES
+  STATUSES,
+  getS3Object,
+  xmlToJson
 }

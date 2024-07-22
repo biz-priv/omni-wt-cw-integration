39fc362917440862666525bcfb605b2dc3312426
Mohammed Sazeed <mohammed.sazeed@bizcloudexperts.com>
2024-07-22 19:51:09 +0530

    feat: changes in the add-milestone

--- src/add-milestone/index.js
+++ src/add-milestone/index.js
@@ -2,45 +2,75 @@
 
 const AWS = require('aws-sdk');
 const { get, includes, isEmpty } = require('lodash');
-const axios = require('axios');
-const xml2js = require('xml2js');
 const moment = require('moment-timezone');
-const { dbQuery, putItem, publishToSNS, getDynamoUpdateParam } = require('../shared/dynamo');
+const { dbQuery, putItem } = require('../shared/dynamo');
 const { xmlToJson, cstDateTime, STATUSES } = require('../shared/helper');
+const {
+  getDataFromCustomerList,
+  queryCreateShipment,
+  getLenovoException,
+  handleError,
+  validateCWResponse,
+  sendToCW,
+  payloadToCWforDelays,
+  payloadToCW,
+  checkExistingRecord,
+} = require('./helper');
 
-const dynamoDB = new AWS.DynamoDB.DocumentClient();
-
-const lenovoCustomerId = process.env.LENOVO_CUST_ID;
-const allowedMilestones = ['PUP', 'TTC', 'DEL'];
-
-const checkExistingRecord = async (orderNo, orderStatusId) => {
-  const statusParams = {
-    TableName: process.env.STATUS_TABLE,
-    KeyConditionExpression: 'OrderNo = :orderNo AND OrderStatusId = :orderStatusId',
-    ExpressionAttributeValues: {
-      ':orderNo': orderNo,
-      ':orderStatusId': orderStatusId,
-    },
-  };
-
-  const recordExisting = await dbQuery(statusParams);
-  return recordExisting.length > 0 && recordExisting[0].Status === STATUSES.SENT;
-};
 module.exports.handler = async (event, context) => {
   try {
     console.info('🚀 ~ file: index.js:15 ~ module.exports.handler= ~ event:', event);
     const record = get(event, 'Records[0]', {});
     const recordBody = JSON.parse(get(record, 'body', ''));
     const message = JSON.parse(get(recordBody, 'Message', ''));
-    console.info('🚀 ~ file: index.js:22 ~ module.exports.handler= ~ message:', message);
-
+    console.info('🚀 ~ file: index.js:26 ~ module.exports.handler= ~ message:', message);
     const dynamoTableName = get(message, 'dynamoTableName', '');
+    const newImage = get(message, 'NewImage', {});
+    const oldImage = get(message, 'OldImage', undefined);
+    const data = AWS.DynamoDB.Converter.unmarshall(newImage);
+    const orderNo = get(data, 'FK_OrderNo', '');
+    console.info('🚀 ~ file: index.js:32 ~ module.exports.handler= ~ orderNo:', orderNo)
+
+    const headerParams = {
+      TableName: process.env.SHIPMENT_HEADER_TABLE,
+      KeyConditionExpression: 'PK_OrderNo = :orderNo',
+      FilterExpression: 'ShipQuote = :shipquote',
+      ExpressionAttributeValues: {
+        ':orderNo': orderNo,
+        ':shipquote': 'S',
+      },
+    };
+
+    const shipmentHeaderResult = await dbQuery(headerParams);
+
+    if (shipmentHeaderResult.length === 0) {
+      return `No data found in ${process.env.SHIPMENT_HEADER_TABLE} for PK_OrderNo: ${orderNo}.`;
+    }
+
+    const billNo = get(shipmentHeaderResult, 'BillNo', '');
+    const housebill = get(shipmentHeaderResult, 'Housebill', '');
+
+    const customerListResult = await getDataFromCustomerList(billNo);
+
+    if (customerListResult.length === 0) {
+      return `Bill-to ${billNo} is not in the customer list.`;
+    }
+
+    const allowedMilestones = get(customerListResult, '[0]AllowedMilestoned', []);
+    const milestoneMapping = get(customerListResult, '[0]MilestonesMapping', {});
+
+    const existingRecord = await queryCreateShipment(housebill);
+
+    if (!existingRecord) {
+      return `This shipment ${orderNo} is not created from this integration`;
+    }
 
     if (dynamoTableName === `omni-wt-rt-shipment-milestone-${process.env.STAGE}`) {
-      await handleShipmentMilestone(message);
+      await handleShipmentMilestone(data, oldImage, allowedMilestones, milestoneMapping, housebill);
     } else if (dynamoTableName === `omni-wt-rt-apar-failure-${process.env.STAGE}`) {
-      await handleAparFailure(message);
+      await handleAparFailure(data, housebill);
     }
+    return STATUSES.SUCCESS;
   } catch (error) {
     console.error('Error in handler:', error);
     await handleError(error, context, event);
@@ -48,10 +78,13 @@ module.exports.handler = async (event, context) => {
   }
 };
 
-const handleShipmentMilestone = async (message) => {
-  const newImage = get(message, 'NewImage', {});
-  const oldImage = get(message, 'OldImage', undefined);
-  const data = AWS.DynamoDB.Converter.unmarshall(newImage);
+async function handleShipmentMilestone(
+  data,
+  oldImage,
+  allowedMilestones,
+  milestoneMapping,
+  housebill
+) {
   const orderNo = get(data, 'FK_OrderNo', '');
   const orderStatusId = get(data, 'FK_OrderStatusId', '');
   const eventDateTime = get(data, 'EventDateTime', '');
@@ -85,17 +118,6 @@ const handleShipmentMilestone = async (message) => {
     return;
   }
 
-  const headerParams = {
-    TableName: process.env.SHIPMENT_HEADER_TABLE,
-    KeyConditionExpression: 'PK_OrderNo = :orderNo',
-    FilterExpression: 'ShipQuote = :shipquote AND BillNo = :billno',
-    ExpressionAttributeValues: {
-      ':orderNo': orderNo,
-      ':shipquote': 'S',
-      ':billno': lenovoCustomerId,
-    },
-  };
-
   const referenceParams = {
     TableName: process.env.REFERENCE_TABLE,
     IndexName: process.env.REFERENCE_TABLE_INDEX,
@@ -108,17 +130,7 @@ const handleShipmentMilestone = async (message) => {
     },
   };
 
-  const [shipmentHeaderResult, referencesResult] = await Promise.all([
-    dbQuery(headerParams),
-    dbQuery(referenceParams),
-  ]);
-
-  if (isEmpty(shipmentHeaderResult)) {
-    console.info(
-      `No data found in ${process.env.SHIPMENT_HEADER_TABLE} table for FK_OrderNo: ${orderNo}.`
-    );
-    return;
-  }
+  const [referencesResult] = await Promise.all([dbQuery(referenceParams)]);
 
   if (isEmpty(referencesResult)) {
     throw new Error(
@@ -126,13 +138,17 @@ const handleShipmentMilestone = async (message) => {
     );
   }
 
-  const housebill = get(shipmentHeaderResult, '[0].Housebill');
   console.info('Housebill:', housebill);
 
   const referenceNo = get(referencesResult, '[0].ReferenceNo', '');
   console.info('Reference No:', referenceNo);
 
-  const cwPayload = await payloadToCW(referenceNo, formattedDateTime, orderStatusId);
+  const cwPayload = await payloadToCW(
+    referenceNo,
+    formattedDateTime,
+    orderStatusId,
+    milestoneMapping
+  );
   console.info('Payload:', cwPayload);
 
   const xmlCWResponse = await sendToCW(cwPayload);
@@ -146,6 +162,7 @@ const handleShipmentMilestone = async (message) => {
     item: {
       OrderNo: orderNo,
       OrderStatusId: orderStatusId,
+      Housebill: housebill,
       ReferenceNo: referenceNo,
       Status: STATUSES.SENT,
       EventDateTime: formattedDateTime,
@@ -154,11 +171,9 @@ const handleShipmentMilestone = async (message) => {
       InsertedTimeStamp: cstDateTime,
     },
   });
-};
+}
 
-const handleAparFailure = async (message) => {
-  const newImage = get(message, 'NewImage', {});
-  const data = AWS.DynamoDB.Converter.unmarshall(newImage);
+async function handleAparFailure(data, housebill) {
   const orderNo = get(data, 'FK_OrderNo', '');
   console.info('🚀 ~ file: index.js:151 ~ module.exports.handler= ~ orderNo:', orderNo);
   const fdCode = get(data, 'FDCode', '');
@@ -180,17 +195,6 @@ const handleAparFailure = async (message) => {
     return;
   }
 
-  const headerParams = {
-    TableName: process.env.SHIPMENT_HEADER_TABLE,
-    KeyConditionExpression: 'PK_OrderNo = :orderNo',
-    FilterExpression: 'ShipQuote = :shipquote AND BillNo = :billno',
-    ExpressionAttributeValues: {
-      ':orderNo': orderNo,
-      ':shipquote': 'S',
-      ':billno': lenovoCustomerId,
-    },
-  };
-
   const referenceParams = {
     TableName: process.env.REFERENCE_TABLE,
     IndexName: process.env.REFERENCE_TABLE_INDEX,
@@ -203,17 +207,7 @@ const handleAparFailure = async (message) => {
     },
   };
 
-  const [shipmentHeaderResult, referencesResult] = await Promise.all([
-    dbQuery(headerParams),
-    dbQuery(referenceParams),
-  ]);
-
-  if (isEmpty(shipmentHeaderResult)) {
-    console.info(
-      `No data found in ${process.env.SHIPMENT_HEADER_TABLE} table for FK_OrderNo: ${orderNo}.`
-    );
-    return;
-  }
+  const [referencesResult] = await Promise.all([dbQuery(referenceParams)]);
 
   if (isEmpty(referencesResult)) {
     throw new Error(
@@ -221,7 +215,6 @@ const handleAparFailure = async (message) => {
     );
   }
 
-  const housebill = get(shipmentHeaderResult, '[0].Housebill');
   console.info('Housebill:', housebill);
 
   const referenceNo = get(referencesResult, '[0].ReferenceNo', '');
@@ -262,6 +255,7 @@ const handleAparFailure = async (message) => {
     item: {
       OrderNo: orderNo,
       OrderStatusId: `DELAY-${fdCode}-${description}`,
+      Housebill: housebill,
       ReferenceNo: referenceNo,
       Status: STATUSES.SENT,
       EventDateTime: formattedDateTime,
@@ -270,269 +264,4 @@ const handleAparFailure = async (message) => {
       InsertedTimeStamp: cstDateTime,
     },
   });
-};
-
-async function payloadToCW(referenceNo, eventDateTime, orderStatusId) {
-  try {
-    const builder = new xml2js.Builder({
-      headless: true,
-      renderOpts: { pretty: true, indent: '    ' },
-    });
-
-    const xmlData = {
-      UniversalEvent: {
-        $: {
-          xmlns: 'http://www.cargowise.com/Schemas/Universal/2011/11',
-          version: '1.1',
-        },
-        Event: {
-          DataContext: {
-            DataTargetCollection: {
-              DataTarget: {
-                Type: 'ForwardingShipment',
-                Key: referenceNo,
-              },
-            },
-          },
-          EventTime: eventDateTime,
-          EventType: ['TTC', 'PUP'].includes(orderStatusId) ? 'PCF' : 'DCF',
-          IsEstimate: 'false',
-        },
-      },
-    };
-
-    return builder.buildObject(xmlData);
-  } catch (error) {
-    console.error('Error in trackingShipmentPayload:', error);
-    throw error;
-  }
-}
-
-async function payloadToCWforDelays(referenceNo, lenovoCode, description) {
-  try {
-    const builder = new xml2js.Builder({
-      headless: true,
-      renderOpts: { pretty: true, indent: '    ' },
-    });
-
-    const xmlData = {
-      UniversalShipment: {
-        $: {
-          'xmlns:ns0': 'http://www.cargowise.com/Schemas/Universal/2011/11',
-        },
-        Shipment: {
-          $: {
-            xmlns: 'http://www.cargowise.com/Schemas/Universal/2011/11',
-          },
-          DataContext: {
-            DataTargetCollection: {
-              DataTarget: {
-                Type: 'ForwardingShipment',
-                Key: referenceNo,
-              },
-            },
-          },
-          NoteCollection: {
-            $: {
-              Content: 'Partial',
-            },
-            Note: {
-              Description: 'Client Visible Job Notes',
-              IsCustomDescription: 'false',
-              NoteText: `${lenovoCode}-${description}`,
-              NoteContext: {
-                Code: 'AAA',
-              },
-            },
-          },
-        },
-      },
-    };
-
-    return builder.buildObject(xmlData);
-  } catch (error) {
-    console.error('Error in trackingShipmentPayload:', error);
-    throw error;
-  }
-}
-
-async function sendToCW(postData) {
-  try {
-    const config = {
-      url: process.env.CW_URL,
-      method: 'post',
-      headers: {
-        Authorization: process.env.CW_ENDPOINT_AUTHORIZATION,
-      },
-      data: postData,
-    };
-
-    console.info('config:', config);
-    const res = await axios.request(config);
-    if (get(res, 'status', '') === 200) {
-      return get(res, 'data', '');
-    }
-    throw new Error(`CARGOWISE API Request Failed: ${res}`);
-  } catch (error) {
-    console.error('CARGOWISE API Request Failed:', error);
-    throw error;
-  }
-}
-
-const validateCWResponse = (response) => {
-  const EventType = get(response, 'UniversalResponse.Data.UniversalEvent.Event.EventType', '');
-  const contentType =
-    get(
-      response,
-      'UniversalResponse.Data.UniversalEvent.Event.ContextCollection.Context',
-      []
-    ).filter((obj) => obj.Type === 'ProcessingStatusCode')[0]?.Value || '';
-
-  if (EventType !== 'DIM' || contentType !== 'PRS') {
-    throw new Error(
-      `CARGOWISE API call failed: ${get(response, 'UniversalResponse.ProcessingLog', '')}`
-    );
-  }
-};
-
-const handleError = async (error, context, event) => {
-  console.error('Error:', error);
-  const record = get(event, 'Records[0]', {});
-  const recordBody = JSON.parse(get(record, 'body', ''));
-  const message = JSON.parse(get(recordBody, 'Message', ''));
-  const data = AWS.DynamoDB.Converter.unmarshall(get(message, 'NewImage', {}));
-  const orderNo = get(data, 'FK_OrderNo', '');
-  let orderStatusId;
-  let fdCode;
-  let lenovoException;
-  let description;
-  if (
-    get(message, 'dynamoTableName', '') === `omni-wt-rt-shipment-milestone-${process.env.STAGE}`
-  ) {
-    orderStatusId = get(data, 'FK_OrderStatusId', '');
-  } else {
-    fdCode = get(data, 'FDCode', '');
-    console.info('🚀 ~ file: index.js:159 ~ module.exports.handler= ~ fdCode:', fdCode);
-    lenovoException = getLenovoException(fdCode);
-    console.info(
-      '🚀 ~ file: index.js:161 ~ module.exports.handler= ~ lenovoException:',
-      lenovoException
-    );
-    description = get(lenovoException, 'description');
-    console.info('🚀 ~ file: test.js:3074 ~ lenovoCode:', description);
-    orderStatusId = `DELAY-${fdCode}-${description}`;
-  }
-
-  try {
-    await sendSNSNotification(context, error, orderNo, orderStatusId);
-    console.info('SNS notification has been sent');
-  } catch (snsError) {
-    console.error('Error while sending SNS notification:', snsError);
-  }
-
-  const errorMsg = `${error.message}`;
-  await updateItem({
-    tableName: process.env.STATUS_TABLE,
-    key: { OrderNo: orderNo, OrderStatusId: orderStatusId },
-    attributes: {
-      Status: STATUSES.FAILED,
-      ErrorMsg: errorMsg,
-      InsertedTimeStamp: cstDateTime,
-    },
-  });
-};
-
-async function updateItem({ tableName, key, attributes }) {
-  const { ExpressionAttributeNames, ExpressionAttributeValues, UpdateExpression } =
-    getDynamoUpdateParam(attributes);
-
-  const params = {
-    TableName: tableName,
-    Key: key,
-    UpdateExpression,
-    ExpressionAttributeNames,
-    ExpressionAttributeValues,
-  };
-
-  try {
-    return await dynamoDB.update(params).promise();
-  } catch (e) {
-    console.error('Update Item Error: ', e, '\nUpdate params: ', params);
-    throw new Error('UpdateItemError');
-  }
-}
-const sendSNSNotification = (context, error, orderNo, orderStatusId) =>
-  publishToSNS({
-    message: `Error in ${context.functionName}.\n\nOrderNo: ${orderNo}.\n\nOrderStatusId: ${orderStatusId}.\nError Details: ${error.message}.`,
-    subject: `LENOVO ADD MILESTONE ERROR ~ OrderNo: ${orderNo} / OrderStatusId: ${orderStatusId}`,
-  });
-
-const exceptionMapping = {
-  ADE: { lenovoCode: 'C05', description: 'EXPEDITE DELIVERY, EARLY ARRIVAL' },
-  APP: { lenovoCode: 'A1', description: 'MISSED DELIVERY' },
-  BADDA: { lenovoCode: 'A51', description: 'CARRIER KEYING ERROR' },
-  CAN: { lenovoCode: 'CO', description: 'ORDER CANCELLED BY CUSTOMER' },
-  CAR: { lenovoCode: 'A91', description: 'MISSED DELIVERY-CARRIER ERROR' },
-  CHO: { lenovoCode: 'A42', description: 'HOLIDAY - CLOSED' },
-  CON: { lenovoCode: 'AG', description: 'CONSIGNEE RELATED' },
-  COS: { lenovoCode: 'S1', description: 'DAMAGED - DELIVERY NOT COMPLETED' },
-  CRD: { lenovoCode: 'B02', description: 'APPOINTMENT OR PRE-ARRANGED DELIVERY DATE' },
-  CUP: { lenovoCode: 'AJ', description: 'OTHER CARRIER RELATED' },
-  CUS: { lenovoCode: 'A40', description: 'SHIPPER RELATED' },
-  CUST: { lenovoCode: 'A40', description: 'SHIPPER RELATED' },
-  DAM: { lenovoCode: 'A10', description: 'DAMAGED - DELIVERY NOT COMPLETED' },
-  DEL: { lenovoCode: 'A33', description: 'OTHER CARRIER RELATED' },
-  DLE: { lenovoCode: 'S01', description: 'INCOMPLETE DELIVERY' },
-  FAOUT: {
-    lenovoCode: 'FOI',
-    description: '3PL OPS/NETWORK RELATED PROBLEM (FORWARDER ORIGIN ISSUE)',
-  },
-  FTUSP: { lenovoCode: 'I01', description: '3PL EDI ISSUE' },
-  HUB: { lenovoCode: 'A33', description: 'OTHER CARRIER RELATED' },
-  ICC: { lenovoCode: 'AQ', description: 'RECIPIENT UNAVAILABLE-DELIVERY DELAYED' },
-  INCOM: { lenovoCode: 'A13', description: 'OTHER' },
-  INMIL: { lenovoCode: 'A51', description: 'CARRIER KEYING ERROR' },
-  INS: { lenovoCode: 'A13', description: 'OTHER' },
-  IRRSL: { lenovoCode: 'A92', description: 'EXCEEDS SERVICE LIMITATION' },
-  LAD: { lenovoCode: 'B08', description: 'IMPROPER UNLOADING FACILITY OR EQUIPMENT' },
-  LATEB: { lenovoCode: 'AW', description: 'PAST CUT-OFF TIME' },
-  LOST: { lenovoCode: 'A5', description: 'UNABLE TO LOCATE' },
-  LPU: { lenovoCode: 'SOS', description: 'ODM/PLANT/MANUFACTURING LATE HANDOVER' },
-  MI: { lenovoCode: 'A30', description: 'MECHANICAL BREAKDOWN' },
-  MISC: { lenovoCode: 'BG', description: 'OTHER' },
-  MISCU: { lenovoCode: 'A95', description: 'PAST CUT-OFF TIME' },
-  MISS: { lenovoCode: 'A5', description: 'UNABLE TO LOCATE' },
-  MTT: { lenovoCode: 'A33', description: 'OTHER CARRIER RELATED' },
-  NTDT: { lenovoCode: 'P01', description: 'PROCESSING DELAY' },
-  OLHNC: { lenovoCode: 'AW', description: 'PAST CUT-OFF TIME' },
-  OLHNF: { lenovoCode: 'A33', description: 'OTHER CARRIER RELATED' },
-  OLHNS: { lenovoCode: 'A98', description: 'FORWARDER CAPACITY ISSUE' },
-  OLHNT: { lenovoCode: 'AV', description: 'EXCEEDS SERVICE LIMITATION' },
-  OMNIF: { lenovoCode: 'P01', description: 'PROCESSING DELAY' },
-  OMNII: { lenovoCode: 'C6', description: 'WAITING SHIPPING INSTRUCTIONS' },
-  PACK: { lenovoCode: 'A13', description: 'OTHER' },
-  PUE: { lenovoCode: 'A40', description: 'SHIPPER RELATED' },
-  PWD: { lenovoCode: 'SOW', description: 'SHIPMENT OVERWEIGHT' },
-  RAD: { lenovoCode: 'BK', description: 'PRE-ARRANGED APPOINTMENT' },
-  REALA: { lenovoCode: 'AJ', description: 'OTHER CARRIER RELATED' },
-  REALR: { lenovoCode: 'AJ', description: 'OTHER CARRIER RELATED' },
-  REFU: { lenovoCode: 'A07', description: 'REFUSED BY CONSIGNEE' },
-  SHI: { lenovoCode: 'AM', description: 'SHIPPER RELATED' },
-  SHORT: { lenovoCode: 'S01', description: 'INCOMPLETE DELIVERY' },
-  SLC: { lenovoCode: 'SLD', description: 'SERVICE LEVEL DECREASE' },
-  SOS: { lenovoCode: 'A43', description: 'WEATHER OR NATURAL DISASTER RELATED' },
-  SSSF: { lenovoCode: 'A12', description: 'PACKAGE SORTED TO WRONG ROUTE' },
-  TFIDI: { lenovoCode: 'A2', description: 'INCORRECT ADDRESS / UNABLE TO LOCATE' },
-  TFMIS: { lenovoCode: 'A2', description: 'INCORRECT ADDRESS / UNABLE TO LOCATE' },
-  THEFT: { lenovoCode: '019', description: 'CASUALTY LOSS' },
-  URC: { lenovoCode: 'A46', description: 'RECIPIENT UNAVAILABLE-DELIVERY DELAYED' },
-  WADD: { lenovoCode: 'A03', description: 'INCORRECT ADDRESS' },
-};
-
-function getLenovoException(wtCode) {
-  const exception = exceptionMapping[wtCode];
-  if (exception) {
-    return exception;
-  }
-  return { lenovoCode: 'Unknown', description: 'No matching Lenovo exception code found' };
 }
